af81c6b2a03e5552e65529766ca253f1f8e5a832
angelycontrerasr <angelycontrerasr@gmail.com>
2025-07-30 16:48:35 -0400

    finished for now

--- src/shoppingCart/Cards.jsx
+++ src/shoppingCart/Cards.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
-
+import styles from "./cards.module.css"
 const Electronic = () => {
     const [products, setProducts] = useState([]); // Store array of products
     const [cartCount, setCartCount] = useState(0); // State for the cart item counter
@@ -24,7 +24,7 @@ const Electronic = () => {
     };
     // Card component to display individual product
     const ProductCard = ({ product }) => (
-        <div style={{background: '#4662AF', border: '1px solid #ccc', margin: '10px', padding: '16px', width: '200px', borderRadius: '4rem'}}>
+        <div style={{fontFamily: 'monospace'  ,background: '#4662AF', border: '1px solid #ccc', margin: '10px', padding: '16px', width: '200px', borderRadius: '4rem'}}>
             {product.thumbnail && (
                 <img
                     src={product.thumbnail}
@@ -32,10 +32,10 @@ const Electronic = () => {
                     style={{width: '100%', height: 'auto', borderRadius: '2rem', background: '#003'}}
                 />
             )}
-            <h3>{product.title}</h3>
+            <h3 >{product.title}</h3>
             <p>Price: ${product.price}</p>
             <p>Quantity: {product.quantity}</p>
-            <button style={{background: '#AAA', borderRadius: '10px'}} onClick={() => addToCart(product)}>
+            <button style={{fontFamily: 'monospace' ,background: '#AAA', borderRadius: '10px'}} onClick={() => addToCart(product)}>
                 Add to Cart
             </button>
         </div>
@@ -60,9 +60,9 @@ const Electronic = () => {
             }}>
                 {cartCount}
             </div>
-            <h1>All Products</h1>
-            <Link to="/">
-                <button>Go Back</button>
+            <h1 style={{fontFamily:'monospace'}}>All Products</h1>
+            <Link to="/" >
+                <button style={{background: '#AAA', borderRadius: '10px', width: '8rem', height: '3rem'}}>Go Back</button>
             </Link>
             <div style={{display: 'flex', flexWrap: 'wrap' ,background: '#547BA1'}}>
                 {/* Map through products and render a card for each */}
